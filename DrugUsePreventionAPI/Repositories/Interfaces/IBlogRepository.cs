dd92d461924ac5bdd374398a2ab304d12a76edb0
YAHsaveme <hoangdvse183095@fpt.edu.vn>
2025-07-02 23:30:24 +0700

    Update Blogs

---
+++ DrugUsePreventionAPI/Repositories/Interfaces/IBlogRepository.cs
@@ -0,0 +1,13 @@
+﻿using DrugUsePreventionAPI.Models.Entities;
+
+namespace DrugUsePreventionAPI.Repositories.Interfaces
+{
+    public interface IBlogRepository
+    {
+        Task<IEnumerable<Blog>> GetAllAsync();
+        Task AddAsync(Blog blog);
+        Task<Blog?> GetByIdAsync(int id);
+        Task UpdateAsync(Blog blog);
+        Task DeleteAsync(Blog blog);
+    }
+}
---
+++ DrugUsePreventionAPI/Services/Implementations/BlogService.cs
@@ -0,0 +1,54 @@
+﻿using DrugUsePreventionAPI.Models.Entities;
+using DrugUsePreventionAPI.Repositories.Interfaces;
+using DrugUsePreventionAPI.Services.Interfaces;
+
+namespace DrugUsePreventionAPI.Services.Implementations
+{
+    public class BlogService : IBlogService
+    {
+        private readonly IBlogRepository _repo;
+
+        public BlogService(IBlogRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<Blog?> GetBlogByIdAsync(int id)
+        {
+            return await _repo.GetByIdAsync(id);
+        }
+        public async Task<IEnumerable<Blog>> GetBlogsAsync()
+        {
+            return await _repo.GetAllAsync();
+        }
+
+        public async Task AddBlogAsync(Blog blog)
+        {
+            blog.Author = null;
+            await _repo.AddAsync(blog);
+        }
+
+        public async Task<bool> UpdateBlogAsync(int id, Blog blog)
+        {
+            var existing = await _repo.GetByIdAsync(id);
+            if (existing == null) return false;
+
+            existing.Title = blog.Title;
+            existing.Content = blog.Content;
+            existing.Status = blog.Status;
+            existing.AuthorID = blog.AuthorID;
+            existing.PublishDate = blog.PublishDate;
+
+            await _repo.UpdateAsync(existing);
+            return true;
+        }
+
+        public async Task<bool> DeleteBlogAsync(int id)
+        {
+            var existing = await _repo.GetByIdAsync(id);
+            if (existing == null) return false;
+
+            await _repo.DeleteAsync(existing);
+            return true;
+        }
+    }
+}
