70ee2a0cb9923dc1c115682fd18885c9f3e6a643
Đỗ Việt Hoàng SE183095 <hoangdvse183095@fpt.edu.vn>
2025-07-02 23:11:25 +0700

    Update Blogs

--- db/DrugUsePreventionDB.sql
+++ db/DrugUsePreventionDB.sql
@@ -397,3 +397,15 @@ BEGIN
 END;
 GO
 EXEC GetConsultantSchedulesWithStatus @ConsultantId = 5, @StartDate = '2025-06-20', @EndDate = '2025-06-30';
+
+-- Thêm Thumbnail và Avatar trong bảng Blogs
+ALTER TABLE Blogs ADD Thumbnail NVARCHAR(500);
+ALTER TABLE Blogs ADD AuthorAvatar NVARCHAR(500);
+
+-- Tạo demo_staff trong bảng Users để tạo Blog
+INSERT INTO Users (userID, UserName, Password, Email, RoleID, Status, CreatedAt, UpdatedAt)
+VALUES (1, 'demo_staff', '123', 'demo_staff@demo.com', 1, 'Active', GETDATE(), GETDATE());
+INSERT INTO Users (userName, password, email, roleID)
+VALUES ('demo_staff1', '123456', 'demo@gmail.com', 2);
+INSERT INTO Users (userName, password, email, fullName, roleID, status)
+VALUES ('demo_staff2', '123456', 'demo_staff@demo.com', N'Nhân viên demo', 3, 'Active');
