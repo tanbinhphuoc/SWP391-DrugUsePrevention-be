3f5a9138813f7c439bab094df4d0b1623c753b3f
hoangdoquoc1 <hoangdoquoc1@gmail.com>
2025-07-18 06:41:25 +0700

    thống kê tổng quan khóa học và sửa lại logic hàm đăng kí

---
+++ DrugUsePreventionAPI/Services/Interfaces/ICourseStatisticsService.cs
@@ -0,0 +1,10 @@
+﻿using DrugUsePreventionAPI.Models.DTOs.StatisticalCourse;
+
+namespace DrugUsePreventionAPI.Services.Interfaces
+{
+    public interface ICourseStatisticsService
+    {
+        Task<CourseOverviewStatisticsDto> GetCourseOverviewStatisticsAsync();
+    }
+
+}
