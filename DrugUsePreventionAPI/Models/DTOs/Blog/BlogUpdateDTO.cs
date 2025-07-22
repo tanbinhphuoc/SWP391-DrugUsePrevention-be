2a5b1149325cddd8860244be3feeb4f7499c05c8
Đỗ Việt Hoàng SE183095 <hoangdvse183095@fpt.edu.vn>
2025-07-23 00:32:45 +0700

    Điều chỉnh update

--- DrugUsePreventionAPI/Models/DTOs/Blog/BlogUpdateDTO.cs
+++ DrugUsePreventionAPI/Models/DTOs/Blog/BlogUpdateDTO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace DrugUsePreventionAPI.Models.DTOs.Blog
 {
     public class BlogUpdateDTO
@@ -6,9 +6,8 @@ namespace DrugUsePreventionAPI.Models.DTOs.Blog
         public int BlogID { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-        public int AuthorID { get; set; }
         public DateTime PublishDate { get; set; }
-        public string Status { get; set; }
+        public string Status { get; set; } 
         public string? Thumbnail { get; set; }
         public string? AuthorAvatar { get; set; }
     }
