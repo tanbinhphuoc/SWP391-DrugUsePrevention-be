f90dbf7415dff3321683eaa8a76130320c817663
Đỗ Việt Hoàng SE183095 <hoangdvse183095@fpt.edu.vn>
2025-07-23 00:33:40 +0700

    Create BlogResponseDTO.cs

---
+++ DrugUsePreventionAPI/Models/DTOs/Blog/BlogResponseDTO.cs
@@ -0,0 +1,14 @@
+namespace DrugUsePreventionAPI.Models.DTOs.Blog
+{
+    public class BlogResponseDTO
+    {
+        public int BlogID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Status { get; set; }
+        public string? Thumbnail { get; set; }
+        public string? AuthorAvatar { get; set; }
+    }
+}
